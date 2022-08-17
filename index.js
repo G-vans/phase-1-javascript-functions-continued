59ce99695113a478e705a1d6ec6e6cc54c024474
G-vans <jevans.otieno@student.moringaschool.com>
2022-08-17 08:59:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+
+}
+console.log(saturdayFun("bathe my dog"));
+console.log(saturdayFun());
+
+//Variable Expression
+const  mondayWork = function(activityMonday="go to the office") {
+    return `This Monday, I will ${activityMonday}.`
+
+}
+mondayWork();
+
+
+function wrapAdjective(wrapper="*") {
+
+    return function (adjective="a hard worker") {
+        return `You are ${wrapper}${adjective}${wrapper}!`
+
+    }
+}
+wrapAdjective();
